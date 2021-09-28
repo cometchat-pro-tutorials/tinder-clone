4aeede79636cf07b2b79338e7f64c6ea66a82afc
hieptl <hieptl.developer@gmail.com>
2021-09-29 00:07:36 +0700

    remove config.js file

--- public/js/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  CometChatAppId: "19444372054f7da8",
-  CometChatRegion: "us",
-  CometChatAuthKey: "46545928524142e5f6e2b3564556d9eaa61813fe",
-  CometChatAPIKey: "f1c6e9eb458d989b2845109f6d44ad634d53aebb"
-};
